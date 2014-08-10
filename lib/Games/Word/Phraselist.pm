e8de36779dd6c5ae59e31231d1cdf8a8e4c8927f
Steven Haryanto <stevenharyanto@gmail.com>
2014-08-10 14:57:13 +0000

    Import of SHARYANTO/Games-Word-Phraselist-KBBI-0.02 from CPAN.
    gitpan-cpan-distribution: Games-Word-Phraselist-KBBI
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHARYANTO/Games-Word-Phraselist-KBBI-0.02.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Games/Word/Phraselist.pm
+++ lib/Games/Word/Phraselist.pm
@@ -1,10 +1,15 @@
 package Games::Word::Phraselist;
 
 our $DATE = '2014-08-10'; # DATE
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
 
 use 5.010001;
 use parent qw(Games::Word::Wordlist);
+
+sub phrases       { my $self = shift; $self->words(@_) }
+sub random_phrase { my $self = shift; $self->random_word(@_) }
+sub each_phrase   { my $self = shift; $self->each_word(@_) }
+sub phrases_like  { my $self = shift; $self->words_like(@_) }
 
 1;
 # ABSTRACT: Manage a list of phrases
@@ -21,7 +26,19 @@ Games::Word::Phraselist - Manage a list of phrases
 
 =head1 VERSION
 
-This document describes version 0.01 of Games::Word::Phraselist (from Perl distribution Games-Word-Phraselist-KBBI), released on 2014-08-10.
+This document describes version 0.02 of Games::Word::Phraselist (from Perl distribution Games-Word-Phraselist-KBBI), released on 2014-08-10.
+
+=head1 METHODS
+
+=head2 new($filename | \@phrases) => obj
+
+=head2 phrases() => int
+
+=head2 random_phrase() => str
+
+=head2 each_phrase
+
+=head2 phrases_like($regex) => list
 
 =head1 HOMEPAGE
 
