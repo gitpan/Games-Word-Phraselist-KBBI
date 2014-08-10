de6f54c78db11e61bd6af63199a3178fc3fd9018
Steven Haryanto <stevenharyanto@gmail.com>
2014-08-10 14:46:53 +0000

    Import of SHARYANTO/Games-Word-Phraselist-KBBI-0.01 from CPAN.
    gitpan-cpan-distribution: Games-Word-Phraselist-KBBI
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Games-Word-Phraselist-KBBI-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Games/Word/Phraselist.pm
@@ -0,0 +1,53 @@
+package Games::Word::Phraselist;
+
+our $DATE = '2014-08-10'; # DATE
+our $VERSION = '0.01'; # VERSION
+
+use 5.010001;
+use parent qw(Games::Word::Wordlist);
+
+1;
+# ABSTRACT: Manage a list of phrases
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Games::Word::Phraselist - Manage a list of phrases
+
+=head1 VERSION
+
+This document describes version 0.01 of Games::Word::Phraselist (from Perl distribution Games-Word-Phraselist-KBBI), released on 2014-08-10.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Games-Word-Phraselist-KBBI>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Games-Word-Phraselist>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Games-Word-Phraselist-KBBI>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
